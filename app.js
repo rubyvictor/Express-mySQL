e4067214940a9ae9ec84f10ec46da04f1909f41f
Victor Lee <myleevictor@gmail.com>
2018-04-23 17:17:42 +0800

    create connection and send query to sql db to find users

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 app.use(morgan("short"));
+
+//setup db connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "Express_mysql"
+});
 
 //specify root route
 app.get("/", (req, res) => {
@@ -13,30 +20,28 @@ app.get("/", (req, res) => {
 
 //test some dummy rest api data
 app.get("/users", (req, res) => {
-  const user1 = { fistName: "Tom", lastName: "Cruise" };
-  const user2 = { firstName: "Tommy", lastName: "HillFinger" };
-  const user3 = { firstName: "Margo", lastName: "Pennelope" };
-
-  res.json({ user1, user2, user3 });
+  console.log(`fetching all users in db`);
+  const queryString = "SELECT * FROM users";
+  connection.query(queryString, (error, users, fields) => {
+    console.log("fetched users successfully");
+    res.json({users});
+  });
 });
 
-app.get("/user/:id",(req,res)=>{
-    console.log(`fetching user with id ${req.params.id}`)
-    
-    const connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        database: "Express_mysql"
-    })
+app.get("/user/:id", (req, res) => {
+  console.log(`fetching user with id ${req.params.id}`);
 
-    const queryString = `SELECT * FROM users where id = ${req.params.id}`
-    connection.query(queryString,[],(error, users,fields)=>{
-        console.log("fetched users successfully")
-        res.json(users)
-    })
-
-})
-
+  const queryString = `SELECT * FROM users where id = ${req.params.id}`;
+  connection.query(queryString, (error, users, fields) => {
+      if (error){
+          console.log("Failed to query for users:" + error)
+          res.sendStatus(500)
+          return
+      }
+    console.log("fetched users successfully");
+    res.json(users);
+  });
+});
 
 //localhost:3003
 app.listen(3003, () => {
