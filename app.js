9d86194aa291a2cf3ddfd17e708ea9c2153a9fdf
Victor Lee <myleevictor@gmail.com>
2018-04-26 18:38:32 +0800

    added form to update current user which required method-override dependency

--- app.js
+++ app.js
@@ -3,10 +3,25 @@ const app = express();
 const morgan = require("morgan");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 app.use(morgan("short"));
 app.use(express.static("./public"));
+app.use(methodOverride("X-HTTP-Method-Override"));
+app.use(methodOverride("_method"));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(
+  methodOverride(function(req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
 //setup db connection
 const getConnection = mysql.createConnection({
   host: "localhost",
@@ -17,6 +32,29 @@ const getConnection = mysql.createConnection({
 //specify root route
 app.get("/", (req, res) => {
   res.send("Hello from express application");
+});
+
+// Update current user
+app.put("/user_update", (req, res) => {
+  console.log("Trying to update a current user");
+
+  const id = req.body.user_id;
+  const firstName = req.body.update_first_name;
+  const lastName = req.body.update_last_name;
+  const queryString = "UPDATE users SET ? WHERE id = ?";
+  getConnection.query(
+    queryString,
+    [{ first_name: firstName, last_name: lastName }, id],
+    (error, results, fields) => {
+      if (error) {
+        console.log("Failed to update user:" + error);
+        res.sendStatus(500);
+        return;
+      }
+      console.log("Successfully updated current user with id:" + results.id);
+      res.json();
+    }
+  );
 });
 
 // Create new user
